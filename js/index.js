ad4c3739c90bbac3a2c6d535fca9210570825dbd
josiahmokob0 <jmnyarega@gmail.com>
2021-06-14 19:41:05 +0300

    custom checkbox

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const check = document.getElementById("theme-switcher");
 const custom = document.getElementById("body");
+const text = document.getElementById("mode");
 
 const change_theme = (value) => {
   if (value) {
@@ -11,6 +12,7 @@ const change_theme = (value) => {
       const darkColor = getComputedStyle(custom).getPropertyValue(color);
       const style = color.replace("--dark-", "--");
       custom.style.setProperty(style, darkColor);
+      text.innerText = "Dark Mode";
     });
   } else {
     const light = Array.from(getComputedStyle(custom)).filter((x) =>
@@ -22,6 +24,7 @@ const change_theme = (value) => {
       const style = color.replace("--light-", "--");
       custom.style.setProperty(style, lightColor);
     });
+    text.innerText = "Light Mode";
   }
 };
 
